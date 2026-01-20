fbe4d997e1167427a9411f6e4d7ec9b734356318
LIKITHA2601 <likithamohan126@gmail.com>
2026-01-20 13:12:40 +0530

    Add files via upload

---
+++ CODE OUTPUT LAB1.py
@@ -0,0 +1,18 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========== RESTART: C:\Users\likit\Desktop\BL.SC.U4AIE24164\CODE.py ===========
+Enter a string:Likitha
+Numbers of vowels: 3
+Number of consonants: 4
+Matrix multiplication result:
+[19, 22]
+[43, 50]
+Number of common elements: 3
+Transpose of matrix:
+[1, 4]
+[2, 5]
+[3, 6]
+Mean: 124.65
+Median: 123.5
+Mode: 114
