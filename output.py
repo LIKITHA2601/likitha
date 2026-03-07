0565dde0f9edd914726a84d0e824837159208b86
LIKITHA2601 <likithamohan126@gmail.com>
2026-03-07 23:40:55 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,41 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=== RESTART: C:/Users/likit/AppData/Local/Programs/Python/Python313/mllab5.py ==
+
+A1: Linear Regression using ONE attribute (Ib → Va)
+Model Coefficient: [1.77146561e-05]
+Model Intercept: -0.005578777982388136
+
+A2: Regression Error Metrics
+Training -> MSE: 0.08277819620163836 RMSE: 0.2877120021855855 R2: 0.0007346114717367014
+Testing  -> MSE: 0.08652731377621475 RMSE: 0.29415525454462776 R2: 0.0007875882665324685
+
+A3: Regression using MULTIPLE attributes
+Multi-feature Metrics -> MSE: 3.52692755353745e-19 RMSE: 5.938794114580375e-10 R2: 1.0
+
+A4: K-Means Clustering (k=2)
+Cluster Centers:
+ [[-4.82393821e+02  3.71313848e+02 -5.83720496e+01  1.49468189e-02
+   8.82288954e-02 -1.03175714e-01]
+ [ 1.99059986e+02 -2.00314112e+02  6.90473622e+01 -1.61152007e-02
+  -3.13780706e-02  4.74932712e-02]]
+Cluster Labels (first 20):
+ [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+
+A5: Clustering Evaluation Scores
+Silhouette Score: 0.35277254053295176
+Calinski-Harabasz Score: 3248.70310166671
+Davies-Bouldin Index: 1.3084352278417541
+
+A6: Distortion Values for Different k
+k = 2, Distortion = 3040082985.65458
+k = 3, Distortion = 2234869863.39781
+k = 4, Distortion = 1557617893.2333527
+k = 5, Distortion = 1068279180.8537674
+k = 6, Distortion = 852567886.0301543
+k = 7, Distortion = 704334543.7244878
+k = 8, Distortion = 612696069.5031933
+k = 9, Distortion = 557345653.4112847
+
+A7: Elbow Method Graph
